e91ed70fd73b3963bcec1502d831fc565a330a07
Ștefan MURARU <muraru.stefaan@gmail.com>
2024-01-26 20:04:13 +0200

    feat: update header & metas

--- src/site.config.ts
+++ src/site.config.ts
@@ -2,11 +2,11 @@ import type { SiteConfig } from "@/types";
 
 export const siteConfig: SiteConfig = {
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
-	author: "Chris Williams",
+	author: "Ştefan MURARU",
 	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
-	title: "Astro Theme Cactus",
+	title: "Ștefan MURARU",
 	// Meta property used as the default description meta property
-	description: "An opinionated starter theme for Astro",
+	description: "Thoughts, stories and ideas from the engineering world.",
 	// HTML lang property, found in src/layouts/Base.astro L:18
 	lang: "en-GB",
 	// Meta property, found in src/components/BaseHead.astro L:42
@@ -20,9 +20,7 @@ export const siteConfig: SiteConfig = {
 			year: "numeric",
 		},
 	},
-	webmentions: {
-		link: "https://webmention.io/astro-cactus.chriswilliams.dev/webmention",
-	},
+	webmentions: undefined,
 };
 
 // Used to generate links in both the Header & Footer.
